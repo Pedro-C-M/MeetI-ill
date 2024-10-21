2c8c3c75d78db3c5564e94a238ca12e6f7153a43
uo288120 <145131850+Pedro-C-M@users.noreply.github.com>
2024-10-21 13:18:45 +0200

    ToolBar para el activityMain diseñada como elemento custom
    Puede ser correcto a futuro meter todos los elementos custom en una carpeta de res, de momento está ahí puesto "custom_toolbar.xml"

--- app/src/main/java/com/example/meet_ill/MainActivity.kt
+++ app/src/main/java/com/example/meet_ill/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.meet_ill
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +18,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
