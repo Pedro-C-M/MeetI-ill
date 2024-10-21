9d928b4df18dfd1bd1de3a445ee615b186a7baa7
uo289630 <uo289630@uniovi.es>
2024-10-22 01:11:37 +0200

    Añado el fragment para editar el perfil.

--- app/src/main/java/com/example/meet_ill/ProfileFragment.kt
+++ app/src/main/java/com/example/meet_ill/ProfileFragment.kt
@@ -16,9 +16,9 @@ class ProfileFragment : Fragment() {
     private lateinit var imgProfile: ImageView
     private lateinit var tvUsername: TextView
     private lateinit var tvRealName: TextView
-    private lateinit var spnPatologia1: Spinner
-    private lateinit var spnPatologia2: Spinner
-    private lateinit var spnPatologia3: Spinner
+    private lateinit var tvPatologia1: TextView
+    private lateinit var tvPatologia2: TextView
+    private lateinit var tvPatologia3: TextView
     private lateinit var btnEditUser: Button
 
     override fun onCreateView(
@@ -32,24 +32,10 @@ class ProfileFragment : Fragment() {
         imgProfile = view.findViewById(R.id.imgProfile)
         tvUsername = view.findViewById(R.id.tvUsername)
         tvRealName = view.findViewById(R.id.tvRealName)
-        spnPatologia1 = view.findViewById(R.id.spnPatologias1)
-        spnPatologia2 = view.findViewById(R.id.spnPatologias2)
-        spnPatologia3 = view.findViewById(R.id.spnPatologias3)
+        tvPatologia1 = view.findViewById(R.id.tvNombrePatologia1)
+        tvPatologia2 = view.findViewById(R.id.tvNombrePatologia2)
+        tvPatologia3 = view.findViewById(R.id.tvNombrePatologia3)
         btnEditUser = view.findViewById(R.id.btnEditUser)
-
-        // Opciones para el spinner
-        val patologias = arrayOf("Diabetes", "Hipertensión", "Asma", "Ninguna")
-
-        // Adaptador para los Spinners
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, patologias)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        // Asignar adaptadores a cada Spinner
-        spnPatologia1.adapter = adapter
-        spnPatologia2.adapter = adapter
-        spnPatologia3.adapter = adapter
-
-        // Cargar datos de usuario si es necesario
 
         return view
     }
